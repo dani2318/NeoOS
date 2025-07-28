9dff73b019c19e8acbeb82817718e3d1f2f3018e
dani23 <giacche.dani@gmail.com>
2025-07-28 13:41:00 +0200

    Started FAT driver

--- src/boot/stage2/stdio.h
+++ src/boot/stage2/stdio.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "x86.h"
 
-
 void putc(char c);
 void puts(const char* str);
+void _cdecl printf(const char* fmt, ...);
+int* printf_number(int* argp, int lenght, bool sign, int radix);
--- src/boot/stage2/x86.asm
+++ src/boot/stage2/x86.asm
@@ -2,6 +2,41 @@
 
 section _TEXT class=CODE
 
+; void _cdecl x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotientOut, uint32_t* reminderOut);
+
+global _x86_div64_32
+_x86_div64_32:
+    push bp
+    mov bp, sp
+
+    push bx
+
+    ; Divide upper 32 bits
+    mov eax, [bp + 8]                       ; eax = upper 32 bits of dividend
+    mov ecx, [bp + 12]                      ; ecx = divisor
+    xor edx, edx
+    div ecx                                 ; eax - quot, edx - reminder
+
+    ; Store upper 32 bits of the quotient
+    mov ebx, [bp + 16]
+    mov [bx + 4], eax
+
+    ; Divide lower 32 bits
+    mov eax, [bp + 4]                       ; eax = lower 32 bits of dividend
+                                            ; edx = old reminder
+    div ecx
+
+    ; Store result
+    mov [bx], eax
+    mov bx, [bp + 18]
+    mov [bx], edx
+    
+    pop bx
+
+    mov sp, bp
+    pop bp
+    ret
+
 global _x86_TTY_PutChar
 _x86_TTY_PutChar:
     push bp
