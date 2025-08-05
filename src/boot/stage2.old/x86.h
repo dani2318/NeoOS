dffd6d55a39dad6b66eaf2eb3387d213db0deabe
dani23 <giacche.dani@gmail.com>
2025-08-05 18:53:04 +0200

    Removed old x86.asm and x86.h files

--- src/boot/stage2.old/x86.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "stdint.h"
-
-void __attribute__((cdecl)) x86_TTY_PutChar(char c, uint8_t page);
-void __attribute__((cdecl)) x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotientOut, uint32_t* reminderOut);
-bool __attribute__((cdecl)) x86_Disk_Reset(uint8_t drive);
-bool __attribute__((cdecl)) x86_Disk_Read(uint8_t drive, uint16_t cylinder,  uint16_t sector, uint16_t head, uint8_t count, void * dataOut);
-bool __attribute__((cdecl)) x86_Disk_GetDriveParams(uint8_t drive, uint8_t* driveTypeOut, uint16_t* cylindersOut, uint16_t* sectorsOut, uint16_t* headsOut);
