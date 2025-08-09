f338315348b1ce8cfa95243530c276c5c50e55ed
dani23 <giacche.dani@gmail.com>
2025-08-09 23:57:50 +0200

    Switched from .bin files to .elf file for kernel

--- src/boot/stage2/string.c
+++ src/boot/stage2/string.c
@@ -40,3 +40,52 @@ int strlen(const char* str){
 
     return len;
 }
+
+int strcmp(const char* a, const char* b){
+    if (a == NULL && b == NULL)
+        return 0;
+
+    if(a == NULL || b == NULL)
+        return -1;
+
+    while(*a && *b && *a == *b){
+        ++a;
+        ++b;
+    }
+
+    return (*a) - (*b);
+}
+
+
+
+wchar_t* UTF16ToCodepoint(wchar_t* string, int* codepoint){
+    int c1 = *string;
+    ++string;
+    if(c1 >= 0xd800 && c1 < 0xdc00){
+        int c2 = *string;
+        ++string;
+        return (wchar_t*) (((c1 & 0x3ff) << 10) + (c2 & 0x3ff) + 0x10000);
+    }
+    *codepoint = c1;
+    return string;
+}
+
+char* CodepointToUTF16(int codepoint, char* stringOut){
+    if(codepoint <= 0x7F){
+        *stringOut = (char)codepoint;
+    }else if (codepoint <= 0x7FF){
+        *stringOut++ = 0xC0 | ((codepoint >> 6) & 0x1F);
+        *stringOut++ = 0x80 | (codepoint & 0x3F);
+    }else if (codepoint <= 0xFFFF){
+        *stringOut++ = 0xE0 | ((codepoint >> 12) & 0xF);
+        *stringOut++ = 0x80 | ((codepoint >> 6) & 0x3F);
+        *stringOut++ = 0x80 | (codepoint & 0x3F);
+    }else if (codepoint <= 0x1FFFFF){
+        *stringOut++ = 0xF0 | ((codepoint >> 18) & 0x7);
+        *stringOut++ = 0x80 | ((codepoint >> 12) & 0x3F);
+        *stringOut++ = 0x80 | ((codepoint >> 6) & 0x3F);
+        *stringOut++ = 0x80 | (codepoint & 0x3F);
+    }
+
+    return stringOut;
+}
