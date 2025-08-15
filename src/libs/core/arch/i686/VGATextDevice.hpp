b10045cdef7152b7c78d67495649fbdae3285b9c
dani23 <giacche.dani@gmail.com>
2025-08-15 02:22:23 +0200

    Fixed broken push

--- src/libs/core/arch/i686/VGATextDevice.hpp
+++ src/libs/core/arch/i686/VGATextDevice.hpp
@@ -8,6 +8,18 @@ namespace arch{
             public:
                 virtual size_t Write(const uint8_t* data, size_t size); 
                 virtual size_t Read(uint8_t* data, size_t size); 
+                void Clear();
+            private:
+                static uint8_t* g_ScreenBuffer;
+                int m_ScreenX;
+                int m_ScreenY;
+                void PutChar(char c);
+                void Scrollback(int lines);
+                void Setcursor(int x, int y);
+                uint8_t GetColor(int x, int y);
+                char GetChar(int x, int y);
+                void PutColor(int x, int y, uint8_t color);
+                void PutChar(int x, int y, char c);
         };
     }
 }
